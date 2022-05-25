8e9a9cd8261b234c4b3b6bd38ce6b78515caf8a7
mathew <mathewjudianto0603@gmail.com>
2022-05-25 22:36:33 +0700

    register page design

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './pages/welcome.dart';
+import './pages/register.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home : Welcome("Mathew")
+      home : Register("Mathew")
     );
   }
 }
---
+++ lib/pages/register.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Register extends StatelessWidget {
+
+  String user;
+  Register(this.user);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vllet',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/background2.png"),
+                fit: BoxFit.cover),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Center(
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Text(
+                      'VLLET',
+                      style: TextStyle(
+                        color: Colors.white, 
+                        fontSize: 50, 
+                        fontWeight: FontWeight.w700,),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "How Should We Call You?",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Center(
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child : TextField(
+                            decoration: InputDecoration(
+                              border: null,
+                              hintText: 'Your Name',
+                              hintStyle: TextStyle(
+                                fontSize: 20,
+                              )
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 10),
+                        child: SizedBox(
+                          height: 50,
+                          width: double.maxFinite,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.black,
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                            ),
+                            onPressed: () {}, 
+                            child: Text(
+                              'Next  -->',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            )
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
